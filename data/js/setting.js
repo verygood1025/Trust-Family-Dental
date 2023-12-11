8e15da1da44dc4358ba2ee81438f1af9e6225c15
verygood1025 <34592434+verygood1025@users.noreply.github.com>
2023-12-11 17:23:25 +0700

    Update v3 Start build

---
+++ data/js/setting.js
@@ -0,0 +1,98 @@
+var $ = jQuery.noConflict();
+
+function check_touch() {
+    if ("ontouchstart" in document.documentElement) {
+        $('body').removeClass("no_touch");
+    } else {
+        $('body').addClass("no_touch");
+    }
+}
+
+function startup_site() {
+    $('.apm-col:not(.col-submit, .col-agree) .wpcf7-form-control-wrap').on('click', function(event) {
+        event.preventDefault();
+        $(this).find('.wpcf7-not-valid-tip').remove();
+        $('.wpcf7-response-output').hide();
+    });
+
+    var y = new Date();
+    $('year').html(y.getFullYear());
+
+
+    if ($(window).scrollTop() > 0) {
+        $('#Top_bar').addClass('is-sticky');
+        $('#back_to_top').addClass('sticky');
+        $('body').addClass('is-sticky');
+    } else {
+        $('#Top_bar').removeClass("is-sticky");
+        $('#back_to_top').removeClass("sticky");
+        $('body').removeClass("is-sticky");
+    }
+
+    $('.error404 .title').html('Page Not Found');
+    $('.error404 .breadcrumbs li:last-child').html('404 - Page Not Found');
+
+
+    $('body:not(.home) #Header').addClass('header-internal');
+}
+
+function container(){
+    var wh = $(window).height() - ($('#Footer').outerHeight() + $('#Header_wrapper').outerHeight());
+    $('body').css('--ct-h',wh+'px');
+}
+
+$(window).ready(function () {
+    check_touch();
+    startup_site();
+    container();
+});
+
+$(window).on('load',function () {
+    $('.kenw-loadding').hide();
+    new WOW().init(); 
+});
+
+$(window).on('scroll', function () {
+    if ($(window).scrollTop() > 0) {
+        $('#Top_bar').addClass('is-sticky');
+        $('#back_to_top').addClass('sticky');
+        $('body').addClass('is-sticky');
+    } else {
+        $('#Top_bar').removeClass("is-sticky");
+        $('#back_to_top').removeClass("sticky");
+        $('body').removeClass("is-sticky");
+    }
+});
+
+$(window).on('resize', function () {
+    check_touch();
+    container();
+});
+
+//some Function
+window.onhashchange = function() {
+    // if($('body.home').length == 0){
+    //     location.reload();
+    // }
+}
+
+//hook class contact form 7
+document.addEventListener( 'wpcf7mailsent', function( event ) {
+    $('.wpcf7-response-output').remove();
+    var booking_id = $('input[name=booking_id]').val();
+    booking_id++;
+    $('input[name=booking_id]').val(booking_id);
+    window.open('/thank-you', '_self');
+}, false );
+document.addEventListener( 'wpcf7invalid', function( event ) {
+    $('.wpcf7-response-output').show();
+    $('.wpcf7-response-output').addClass('alert alert-danger');
+}, false );
+document.addEventListener( 'wpcf7spam', function( event ) {
+    $('.wpcf7-response-output').show();
+    $('.wpcf7-response-output').addClass('alert alert-warning');
+}, false );
+document.addEventListener( 'wpcf7mailfailed', function( event ) {
+    $('.wpcf7-response-output').show();
+    $('.wpcf7-response-output').addClass('alert alert-warning');
+}, false );
---
+++ option2/assets/libary/poppup/readme.txt
@@ -0,0 +1,8 @@
+demo
+<a class="poppup-link" href="javascript:void(0)" data-target="#footer-maps" data-width="" data-height="" data-bg="#fff" data-color="" data-padding="">
+    <img src="/wp-content/uploads/2022/07/location-icon.png" alt="location">
+    <span>Find Us On Map</span>
+</a>
+<div class="data-popup" id="footer-maps">
+	<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d765.624275686605!2d-105.0637201!3d39.8631017!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876b89bb948ec6e7%3A0x804079dbfa80bd10!2s6030%20W%2092nd%20Ave%20%23300%2C%20Westminster%2C%20CO%2080031%2C%20USA!5e0!3m2!1sen!2s!4v1659289172202!5m2!1sen!2s" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
