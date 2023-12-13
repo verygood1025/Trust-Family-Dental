58c3b881d9fdc0bbeb0cb28828e7996edba1ac49
Phan Thac <phanthac.fata@gmail.com>
2023-12-13 07:35:29 +0700

    push code 12/12/2023

---
+++ option1/assets/js/dev-script.js
@@ -0,0 +1,47 @@
+fixed_social = [
+    {
+        'icon' : 'assets/images/phone-icon.png',
+        'link' : '#',
+        'title' : 'Call',
+        'class' : 'phone-icon show-all',
+        'target' : '_blank',
+    },
+    {
+        'icon' : 'assets/images/fb-icon.png',
+        'link' : '#',
+        'title' : 'Facebook',
+        'class' : 'fb-icon show-mob',
+        'target' : '_blank',
+    },
+    {
+        'icon' : 'assets/images/yelp-icon.png',
+        'link' : '#',
+        'title' : 'Yelp',
+        'class' : 'yelp-icon show-mob',
+        'target' : '_blank',
+    },
+    {
+        'icon' : 'assets/images/ig-icon.png',
+        'link' : '#',
+        'title' : 'Instagram',
+        'class' : 'ig-icon show-mob',
+        'target' : '_blank',
+    },
+    {
+        'icon' : 'assets/images/gg-icon.png',
+        'link' : '#',
+        'title' : 'Google',
+        'class' : 'gg-icon show-mob',
+        'target' : '_blank',
+    },
+]
+
+$(window).ready(function(){
+	if(fixed_social.length > 0){
+		let social_html = '';
+		$.each(fixed_social, function(i, item){
+			social_html += `<li class="${item.class}"><a href="${item.link}" target="${item.target}"><img src="${item.icon}"></a></li>`;
+		});
+		$('.fixed-socials ul').html(social_html);
+	}
+})
